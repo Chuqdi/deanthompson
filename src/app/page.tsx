6507c82e792acf14692b71c28134480a61a9962a
Chuqdi <morganhezekiah111@gmail.com>
2024-09-08 15:18:56 +0100

    fjfj

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,6 @@ export default function Home() {
   return (
     <main className=" bg-black">
       <BannerSection />
-      
       <ResumeSection />
       <ExperienceSection />
       <TestmoniesSection />
